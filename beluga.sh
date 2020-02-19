c9a3ad0265dd02d295dfcf8a0435a73213d9a639
vict0rsch <vsch@pm.me>
2020-02-19 14:35:49 -0500

    working status

--- beluga.sh
+++ beluga.sh
@@ -4,26 +4,29 @@
 #SBATCH --gres=gpu:1                     # Ask for 1 GPU
 #SBATCH --mem=32G                        # Ask for 32 GB of RAM
 #SBATCH --time=24:00:00                   # The job will run for 3 hours
-#SBATCH -o /scratch/vsch/slurm-%j.out  # Write the log in $SCRATCH
+#SBATCH -o /scratch/vsch/continual/slurm-%j.out  # Write the log in $SCRATCH
+#SBATCH --qos unkillable
 
 # 1. Create your environement locally
 module load python/3.7.4
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
+module load httpproxy
 
 cd /home/vsch/continual-translation
-pip install --no-index torch torchvision numpy comet_ml
+# zip -r $SCRATCH/ct-env.zip ct-env > /dev/null #! uncomment to load new packages
+cp /$SCRATCH/ct-env.zip $SLURM_TMPDIR
+unzip $SLURM_TMPDIR/ct-env.zip -d $SLURM_TMPDIR > /dev/null
+source $SLURM_TMPDIR/ct-env/bin/activate
 
 # 2. Copy your dataset on the compute node
 # IMPORTANT: Your dataset must be compressed in one single file (zip, hdf5, ...)!!!
 cp /scratch/vsch/continual/s2w.zip $SLURM_TMPDIR
 
 # 3. Eventually unzip your dataset
-unzip $SLURM_TMPDIR/s2w.zip -d $SLURM_TMPDIR
+unzip $SLURM_TMPDIR/s2w.zip -d $SLURM_TMPDIR > /dev/null
 
 # 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
 #    and look for the dataset into $SLURM_TMPDIR
-python train.py --dataroot $SLURM_TMPDIR --name base_cyclegan_WS --model cycle_gan
+python train.py --dataroot $SLURM_TMPDIR --name base_cyclegan_WS --model cycle_gan --checkpoints_dir "/scratch/vsch/continual/checkpoints" --display_freq 1000 --batch_size 3
 
 # 5. Copy whatever you want to save on $SCRATCH
 cp $SLURM_TMPDIR/<to_save> $SCRATCH
--- util/util.py
+++ util/util.py
@@ -18,7 +18,7 @@ def tensor2im(input_image, imtype=np.uint8):
         else:
             return input_image
         image_numpy = (
-            image_tensor[0].cpu().float().numpy()
+            image_tensor.cpu().float().numpy()
         )  # convert it into a numpy array
         if image_numpy.shape[0] == 1:  # grayscale to RGB
             image_numpy = np.tile(image_numpy, (3, 1, 1))
